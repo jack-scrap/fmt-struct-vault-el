eac22ba8166a0d9688a0f241657a49778b5cd8f4
jackhasakeyboard <jackhasakeyboard@gmail.com>
2024-08-10 16:29:46 -0600

    Generated serialized item that's specific to the vault, taking another item that would be for another list

--- main.c
+++ main.c
@@ -1,3 +1,27 @@
+#include <stdio.h>
+
+const char* name = "vault";
+
+const char postfix = ':';
+
+void err(char* msg) {
+	fprintf(stderr, "Error: %s\n", msg);
+}
+
 int main(int argc, char* argv[]) {
+	if (argc == 1) {
+		err("No arguments");
+
+		return 1;
+	}
+
+	if (argc != 1 + 1) {
+		err("Wrong number of arguments");
+
+		return 1;
+	}
+
+	printf("%s%c%s", name, postfix, argv[1]);
+
 	return 0;
 }
